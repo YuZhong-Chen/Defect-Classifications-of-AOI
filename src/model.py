483e625c54406d44697d92ff5c3e27a3cc65f33a
YuZhong-Chen <yuzhong1214@gmail.com>
2024-12-12 00:01:36 +0800

    feat: Init the model and change the activation func.

--- src/model.py
+++ src/model.py
@@ -5,25 +5,38 @@ from pathlib import Path
 
 
 class SimpleModel(nn.Module):
-    def __init__(self, num_classes: int = 6):
+    def __init__(self, num_classes: int = 6, dropout_rate: float = 0.5):
         super(SimpleModel, self).__init__()
-        
+
         self.input_size = (1, 512, 512)
         self.features = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=7, stride=3, padding=3),  # 32x171x171
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(32, 64, kernel_size=7, stride=3, padding=3),  # 64x57x57
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(64, 128, kernel_size=7, stride=3, padding=3),  # 128x19x19
-            nn.ReLU(),
+            nn.LeakyReLU(),
         )
         self.classifier = nn.Sequential(
-            nn.Linear(46208, 128),
-            nn.ReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(46208, 1024),
+            nn.LeakyReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(1024, 128),
+            nn.LeakyReLU(),
             nn.Linear(128, num_classes),
         )
 
-    def forward(self, x):
+        # Initialize the network
+        self.InitNetwork()
+
+    def InitNetwork(self) -> None:
+        for layer in (self.features, self.classifier):
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight, gain=nn.init.calculate_gain("leaky_relu"))
+                nn.init.zeros_(layer.bias, 0)
+
+    def forward(self, x) -> torch.Tensor:
         x = self.features(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
